c437f0f83dcc687f9bbd0a5c7559a06c7a3ea0a0
ShirishDX007 <140408381+ShirishDX007@users.noreply.github.com>
2024-11-19 11:40:53 +0530

    Lambda,  recursive function added

--- functions/Recursive_function.py
+++ functions/Recursive_function.py
@@ -1,3 +1,12 @@
+#pseudocode for recursive function
+#1. define factorial function with num as an argument
+#2. create a call stack list
+#3. check if num is equal to 1
+#4. if num is equal to 1 then print base value reached and return 1
+#5. else append the input value to call stack list
+#6. print the call stack
+#7. return num multiply by factorial of num -1
+
 def factorial(num):
     call_stack=[]
     if num == 1:
@@ -8,7 +17,7 @@ def factorial(num):
         print('call stack: ', call_stack)
         return num * factorial(num -1)
 
-factorial(5)
+print(f"factorial : ",factorial(5))
 
     
           
